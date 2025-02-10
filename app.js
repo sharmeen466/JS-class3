c5c8adadc6e5c59ae5bf619872301e21d3d9d91a
sharmeen466 <sharmeennazeer2@gmail.com>
2025-02-10 10:18:50 +0500

    java script

---
+++ app.js
@@ -0,0 +1,62 @@
+///   Q   #   1
+
+
+// let userCity = "karachi"
+
+// let user = prompt("Enter your city name", "karachi")
+
+// if (user === userCity){
+// document.write("welcome to the city of light")
+// }
+
+// else {
+//     document.write("welcome to our city")
+// }
+
+
+
+//    Q    #    2
+
+
+// let gender = "male"
+
+// let usergender = prompt("what's ypur gender?")
+
+// if(usergender === gender){
+// console.log("Good Morning Sir!");
+
+// }
+// else {
+//     console.log("Good Morning Ma'am");
+
+// }
+
+
+//    Q    #    3
+
+
+
+// let trafficSignal = prompt("what signal-color do you see?", "Red / Yellow / Green")
+
+// if (trafficSignal === "Red") {
+//     console.log("Must Stop !");
+
+// }
+
+// else {
+//     if (trafficSignal === "Yellow")
+//         console.log("Ready to move");
+//     else {
+//         if (trafficSignal === "Green")
+//             console.log("Move now");
+//         else {
+//             console.log("Invalid color! Please enter Red, Yellow, or Green.");
+
+//         }
+//     }
+// }
+
+
+//    Q    #    4
+
+
